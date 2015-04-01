77b5d9b43f86b944e39ea54b6c9dd3d7e2795ccb
Michael Waterworth <mike@3equals.co.uk>
2015-04-01 01:13:32 +0100

    Remove unused imports Add comments to code

--- src/java/ChatServer/ChatSvr.java
+++ src/java/ChatServer/ChatSvr.java
@@ -5,7 +5,11 @@
  */
 package ChatServer;
 
-import static java.lang.Thread.sleep;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -14,6 +18,8 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import uk.co.threeequals.webchat.*;
 
 /**
@@ -24,9 +30,43 @@ import uk.co.threeequals.webchat.*;
 public class ChatSvr {
     private Group gr = new Group();
     private final HashMap clients = new HashMap();
+    
+    /**
+     * Read a files first line given URL string
+     * @param path path to file as string
+     * @param encoding character encoding to use
+     * @return String containing value of first line
+     * @throws IOException 
+     */
+    String readFile(String path, Charset encoding) throws IOException {
+        BufferedReader br;
+        br = new BufferedReader(
+                new InputStreamReader(
+                        Thread.currentThread().getContextClassLoader().getResource("password.txt").openStream()
+                )
+        );
+        return br.readLine();
+    }
+    
+    /**
+     * Set password used by application group.
+     * Read from file
+     */
+    private void setPassword(){
+        try {
+            String adminPassword = readFile("password.txt", StandardCharsets.UTF_8);
+            gr.setPassword(adminPassword);//Add admin password to Group
+            System.out.println("Password set as: " + adminPassword);
+        } catch (IOException ex) {
+            Logger.getLogger(Group.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
     /**
      * Web service operation
+     * @param messageStr String message sent by client
+     * @param uuid Unique ID of client
+     * @return boolean success value for posting of message
      */
     @WebMethod(operationName = "sendMessage")
     public Boolean sendMessage(@WebParam(name = "messageStr") String messageStr, @WebParam(name = "uuid") String uuid) {
@@ -45,10 +85,12 @@ public class ChatSvr {
     }
 
     /**
-     * Web service operation
+     * Join the group as a client
+     * @return UUID to use as a client with every subsequent request
      */
     @WebMethod(operationName = "join")
     public String join() {
+        setPassword();
         //Create a unique identifier for the application
         UUID uuid = UUID.randomUUID();
         String uuidStr = uuid.toString();
@@ -62,8 +104,8 @@ public class ChatSvr {
     }
 
     /**
-     * Web service operation
-     * @param uuid
+     * Get message from user's message queue
+     * @param uuid Unique ID of user - Random string as shared-secret
      */
     @WebMethod(operationName = "getMessage")
     public Message getMessage(@WebParam(name = "uuid") String uuid) throws InterruptedException {
@@ -86,8 +128,8 @@ public class ChatSvr {
     }
 
     /**
-     * Web service operation
-     * @param uuid
+     * Logout from service
+     * @param uuid unique id of user to logout
      */
     @WebMethod(operationName = "logout")
     public void logout(@WebParam(name = "uuid") String uuid) {
@@ -100,6 +142,10 @@ public class ChatSvr {
         }
     }
     
+    /**
+     * Update a users timeout Date - if too long in the past will be removed
+     * @param uuid unique id of user
+     */
     private void updateClientTimeout(String uuid){
         ClientHandler ch = (ClientHandler) clients.get(uuid);
             if(ch != null){
@@ -107,6 +153,9 @@ public class ChatSvr {
             }
     }
     
+    /**
+     * Remove old stale connections from pool.
+     */
     private void checkClientTimeout(){
         Iterator it = clients.entrySet().iterator();
         while (it.hasNext()) {
